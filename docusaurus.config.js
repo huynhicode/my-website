70f55487a1e25d2230062f4f10b39471836e5e21
steph <steph@huynhicode.dev>
2022-04-13 15:31:34 -0700

    add show last update author

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,6 +26,7 @@ const config = {
           // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
         },
         blog: {
           showReadingTime: true,
